02b002271138f8c5f99e1ef9191fe463f087a244
Talhakiris <talhakiriiss@gmail.com>
2026-01-02 16:51:32 +0300

    Implement scalar functions: abs, upper, lower, reverse (#61)
    * Implement scalar functions: abs, upper, lower, reverse
    
    Closes #32
    
    * Update cratedb_django/models/functions.py
    
    Co-authored-by: Ivan <surister98@gmail.com>
    
    ---------
    
    Co-authored-by: Ivan <surister98@gmail.com>

--- cratedb_django/models/functions.py
+++ cratedb_django/models/functions.py
@@ -10,3 +10,28 @@ class UUID(Func):
     output_field = TextField(
         max_length=20
     )  # the length of a CrateDB random uid.
+
+class Abs(Func):
+    """
+    https://cratedb.com/docs/crate/reference/en/latest/general/builtins/scalar-functions.html#abs
+    """
+    function = "abs"
+
+class Upper(Func):
+    """
+    https://cratedb.com/docs/crate/reference/en/latest/general/builtins/scalar-functions.html#upper
+    """
+    function = "upper"
+
+class Lower(Func):
+    """
+    https://cratedb.com/docs/crate/reference/en/latest/general/builtins/scalar-functions.html#lower
+    """
+    function = "lower"
+
+class Reverse(Func):
+    """
+    https://cratedb.com/docs/crate/reference/en/latest/general/builtins/scalar-functions.html#reverse
+    """
+    function = "reverse"
+
