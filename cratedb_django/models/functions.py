ae85a8f660e0915fed7992ff00606b8c9fef4d16
surister <surister98@gmail.com>
2026-01-05 15:46:05 +0100

    feat: Add support for literal keyword functions

--- cratedb_django/models/functions.py
+++ cratedb_django/models/functions.py
@@ -1,13 +1,37 @@
 from django.db.models.expressions import Func
 
-from cratedb_django.fields import TextField
+from cratedb_django import fields
+
+
+class LiteralKeywordMixin:
+    """
+    Returns the `function` as a keyword literal as opposed to a function. This
+    is compatible as a `db_default` and as an expression in generated fields.
+
+    Useful for easily creating scalar functions with just a name, for example:
+    `current_date` or `current_timestamp`
+    """
+
+    def db_default(self):
+        return self.function
+
+    def resolve_expression(
+        self,
+        query=None,
+        allow_joins=True,
+        reuse=None,
+        summarize=False,
+        for_save=False,
+    ):
+        members = {"as_sql": lambda *_: (self.function, [])}
+        return type("LiteralKeyword", (), members)
 
 
 class UUID(Func):
     """https://cratedb.com/docs/crate/reference/en/latest/general/builtins/scalar-functions.html#gen-random-text-uuid"""
 
     function = "gen_random_text_uuid"
-    output_field = TextField(
+    output_field = fields.TextField(
         max_length=20
     )  # the length of a CrateDB random uid.
 
@@ -52,3 +76,12 @@ class Format(Func):
     """https://cratedb.com/docs/crate/reference/en/latest/general/builtins/scalar-functions.html#format-format-string-parameter-parameter"""
 
     function = "format"
+
+
+class CURRENT_DATE(LiteralKeywordMixin, Func):
+    """
+    https://cratedb.com/docs/crate/reference/en/latest/general/builtins/scalar-functions.html#current-date
+    """
+
+    function = "CURRENT_DATE"
+    output_field = fields.CharField()
